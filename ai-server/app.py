68466f5b6604f7b02e0b69ae81b32160962cef01
Yuriy Guts <yuriy.guts@gmail.com>
2017-10-24 15:28:41 +0300

    Add request timing log to AI server

--- ai-server/app.py
+++ ai-server/app.py
@@ -1,12 +1,25 @@
 import sys
+import time
 
-from flask import Flask, request, jsonify
+from flask import Flask, g, request, jsonify
 from libcheckers.serialization import load_board, load_player, save_move
 
 from ai import pick_next_move
 
 
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    g.request_start_time = time.time()
+
+
+@app.teardown_request
+def teardown_request(exception=None):
+    request_serving_time = time.time() - g.request_start_time
+    with open('request-timing.csv', 'a') as rtf:
+        rtf.write('{0:.5f}\n'.format(request_serving_time))
 
 
 @app.route('/move', methods=['POST'])
@@ -26,4 +39,4 @@ def move():
 
 if __name__ == '__main__':
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
-    app.run(port=port, debug=True)
+    app.run(port=port)
